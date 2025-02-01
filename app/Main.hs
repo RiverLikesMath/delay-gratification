2d53cfa35fb25084401a2a7f5f0cbfb97fb87a62
River Likes Math <river@likesmath.com>
2025-01-31 16:04:59 -0800

    start figuring out data types that SQL will feed into, make a mock version of it using lists, test it a bit

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Data.Time
 import Options.Applicative 
 import TimeLogic 
+import Types
 
 data DelayOpts = DelayOpts 
   {
@@ -32,10 +33,33 @@ parseDateAndGo :: String -> IO () --better name please!
 parseDateAndGo date = do
    now <- getCurrentTime  
    timezone <- getCurrentTimeZone --this'll never be run outside the pacific timezones, right? 
+   now2 <- getCurrentTime
+   
+   fancy now 
+   fancy now2 
+
+   let   start :: DelayedDateTimeStore = DelayedDateTimeStore [] 
+   print start 
+   
+   let next = add TestingStore (DelayedDateTime {dtId =0, name = "First time", beginTime = now})  start
+   putStrLn "next is"
+   print next
+   
+   let next2 = add TestingStore (DelayedDateTime {dtId =1, name = " second time", beginTime = now2}) next
+   print "after that is" 
+   print next2
+
    let startTime = convToUTC date timezone 
       in case startTime of 
         Nothing -> putStrLn "when calling with --start, the date format is YYYY-MM-DD HH:MM. Assumes local time." 
         Just x -> printCheckTimes now x timezone
+
+fancy :: UTCTime  -> IO()  
+fancy now = extrafancy now 
+
+
+extrafancy :: UTCTime -> IO ()
+extrafancy now = print  DelayedDateTime { dtId = 0, name = "First time", beginTime =  now }
 
 trySayTimes :: DelayOpts -> IO () 
 trySayTimes (DelayOpts date) =  parseDateAndGo date 
--- app/Types.hs
+++ app/Types.hs
@@ -1,3 +1,26 @@
+module Types where 
+
+import Data.Time 
+
 data DelayedDateTime = DelayedDateTime { 
-   startTime :: UTCTime
-} 
+   dtId :: Int, 
+   name :: String,
+   beginTime :: UTCTime 
+} deriving (Eq, Show) 
+
+-- Class is a java interface 
+-- algebraic data type 
+
+
+data DelayedDateTimeStore = DelayedDateTimeStore [DelayedDateTime] deriving Show
+
+class DateTimeCrud a where    --a can be any type! so the tuple for data DelayedDateTimeStore up above *might* be better . If we do that we may not need the typeclass at all
+    add    :: a -> DelayedDateTime -> DelayedDateTimeStore -> DelayedDateTimeStore  
+    remove :: a -> DelayedDateTime -> DelayedDateTimeStore -> DelayedDateTimeStore
+    
+data TestingStore = TestingStore 
+
+instance DateTimeCrud TestingStore  where 
+   add storeType dateTime (DelayedDateTimeStore store)    = DelayedDateTimeStore (   store ++ [dateTime] )
+   remove storeType dateTime (DelayedDateTimeStore [])    = DelayedDateTimeStore []
+   remove storeType dateTime (DelayedDateTimeStore store) = DelayedDateTimeStore ( filter (/= dateTime) store )
